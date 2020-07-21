7ca9a7a5fa6cc9a32faee3061e448c11005b667a
lira71 <lida.lidina.93@mail.ru>
2020-07-21 19:13:26 +0300

    create: add ya api map

--- js/main.js
+++ js/main.js
@@ -12,3 +12,18 @@ var mySwiper = new Swiper('.swiper-container', {
         onlyInViewport: false,
     },
 })
+ymaps.ready(init);
+
+function init() {
+    var myMap = new ymaps.Map("ya_map", {
+        center: [41.04388125, 28.99894122],
+        zoom: 15
+    }, {
+        searchControlProvider: 'yandex#search'
+    });
+    myMap.geoObjects
+        .add(new ymaps.Placemark([41.04388125, 28.99894122], {
+            balloonContent: '<strong>GRAND HILTON HOTEL</strong>'}, {
+            preset: 'islands#greenDotIconWithCaption'
+        })); 
+}
