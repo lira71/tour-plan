61acadc4097cfb385bcbf4e852765413a825f7c4
lira71 <lida.lidina.93@mail.ru>
2020-07-23 16:34:56 +0300

    add google map

--- js/main.js
+++ js/main.js
@@ -12,21 +12,7 @@ var hotelSlider = new Swiper('.hotel-slider', {
         onlyInViewport: false,
     },
 });
-ymaps.ready(init);
 
-function init() {
-    var myMap = new ymaps.Map("ya_map", {
-        center: [41.04388125, 28.99894122],
-        zoom: 15
-    }, {
-        searchControlProvider: 'yandex#search'
-    });
-    myMap.geoObjects
-        .add(new ymaps.Placemark([41.04388125, 28.99894122], {
-            balloonContent: '<strong>GRAND HILTON HOTEL</strong>'}, {
-            preset: 'islands#greenDotIconWithCaption'
-        })); 
-};
 var reviewsSlider = new Swiper('.reviews-slider', {
     // Optional parameters
     loop: true,
