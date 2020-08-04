e5722d6798fae6059379e9783434af8aaf9d703d
lira71 <lida.lidina.93@mail.ru>
2020-08-04 15:30:51 +0300

    fix map cursor error

--- js/scripts.js
+++ js/scripts.js
@@ -19,36 +19,6 @@ var hotelSlider = new Swiper(".hotel-slider", {
     onlyInViewport: false,
   },
 });
-//Подключение API Яндекс Карты
-ymaps.ready(function () {
-  var myMap = new ymaps.Map(
-      "map",
-      {
-        center: [12.934948, 100.88328],
-        zoom: 15.3,
-      },
-      {
-        searchControlProvider: "yandex#search",
-      }
-    ),
-    MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
-      '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
-    ),
-    myPlacemark = new ymaps.Placemark(
-      myMap.getCenter(),
-      {
-        hintContent: "Grand Hilton Hotel",
-      },
-      {
-        iconLayout: "default#image",
-        iconImageHref: "js/location-icon.svg",
-        iconImageSize: [40, 52],
-        iconImageOffset: [-25, -40],
-      }
-    );
-  myMap.geoObjects.add(myPlacemark);
-  myMap.behaviors.disable("scrollZoom");
-});
 
 //Cоздание parallax эффекта newsletter
 $(".newsletter").parallax({ imageSrc: "img/newsletter/newsletter-bg.jpg" });
